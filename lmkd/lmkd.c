5c3231a6c553783701ffea1f463b01b7e0aedf7c
Tim Murray <timmurray@google.com>
2018-10-25 17:05:41 -0700

    DO NOT MERGE: lmkd: retune rate at which processes are killed
    Kill a single process at a time and try to wait up to 100ms for
    that process to reclaim memory before triggering another kill.
    
    Test: boots, works
    bug: 116877958
    Change-Id: I6775d0534b3e3728c04389d3eae1a00e3cbf9f27
    (cherry picked from commit afb3a15f39e73978c071a58d3466993293c6d3b1)

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1337,7 +1337,7 @@ do_kill:
             min_score_adj = level_oomadj[level];
         }
 
-        pages_freed = find_and_kill_processes(min_score_adj, pages_to_free);
+        pages_freed = find_and_kill_processes(min_score_adj, 0);
 
         if (pages_freed == 0) {
             /* Rate limit kill reports when nothing was reclaimed */
